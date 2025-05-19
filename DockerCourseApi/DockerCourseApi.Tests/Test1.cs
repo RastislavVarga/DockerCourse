8bfae1510d71980cd065e2299498447ceb5817ed
Rastislav Varga <vargara@softec.sk>
2025-05-19 23:43:00 +0200

    my first commit

---
+++ DockerCourseApi/DockerCourseApi.Tests/Test1.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using FluentAssertions;
+
+namespace DockerCourseApi.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        var httpClient = new CustomWebApplicationFactory().CreateClient();
+        var response = await httpClient.GetAsync("/podcasts");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
